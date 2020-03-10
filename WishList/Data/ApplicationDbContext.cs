5d9599b2509ae69d44267a1d3a5e48fe700c2efd
pettterbackman <60734617+pettterbackman@users.noreply.github.com>
2020-03-10 08:23:30 +0100

    Completed module

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items;
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+
+}
